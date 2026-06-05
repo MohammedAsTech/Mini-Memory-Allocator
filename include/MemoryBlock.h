bca0f38d1d5d2ecaa708699c94683ac9ff987725
MohammedAsTech <mmm579aiman@gmail.com>
2026-06-05 18:39:05 +0300

    adding MemoryBlock.h

---
+++ include/MemoryBlock.h
@@ -0,0 +1,38 @@
+//
+// Created by moham on 05/06/2026.
+//
+
+#ifndef MEMORYBLOCK_H
+#define MEMORYBLOCK_H
+class MemoryBlock {
+private:
+    int id;
+    int start;
+    int size;
+    bool isFree;
+public:
+    MemoryBlock(int id,int start,int size,bool isFree):id(id),start(start)
+    ,size(size),isFree(isFree){}
+
+    ~MemoryBlock() = default;
+
+    int getId() const;
+
+    int getStart() const;
+
+    int getSize() const;
+
+    bool getIsFree() const;
+
+    void setId(int val);
+
+    void setSize(int val);
+
+    void setStart(int val);
+
+    void SetIsFree(bool val);
+
+
+};
+
+#endif //MEMORYBLOCK_H
---
+++ src/MemoryBlock.cpp
@@ -0,0 +1,4 @@
+#include "MemoryBlock.h"
+//
+// Created by moham on 05/06/20
+
