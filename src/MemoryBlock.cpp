203363faefb4594ebd42b14626a471dc3b2104dd
MohammedAsTech <mmm579aiman@gmail.com>
2026-06-05 18:43:37 +0300

    implementing getters and setters in MemoryBlock.cpp

--- src/MemoryBlock.cpp
+++ src/MemoryBlock.cpp
@@ -1,4 +1,33 @@
 #include "MemoryBlock.h"
 //
 // Created by moham on 05/06/20
+int MemoryBlock::getSize() const {
+    return size;
+}
+int MemoryBlock::getId() const {
+    return id;
+}
+int MemoryBlock::getStart() const {
+    return start;
+}
+void MemoryBlock::setId(int val) {
+    id = val;
+}
+void MemoryBlock::setSize(int val) {
+    size = val;
+}
+void MemoryBlock::setStart(int val) {
+    start = val;
+}
+void MemoryBlock::SetIsFree(bool val) {
+    isFree = val;
+}
+bool MemoryBlock::getIsFree() const {
+    return isFree;
+}
+
+
+
+
+
 
