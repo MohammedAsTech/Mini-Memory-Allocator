ff2b320a5599ee18de90adbf69fdf62ae6167072
MohammedAsTech <mmm579aiman@gmail.com>
2026-06-07 18:52:57 +0300

    adding Command classes using command desgin pattern

---
+++ include/Commands.h
@@ -0,0 +1,49 @@
+//
+// Created by moham on 07/06/2026.
+//
+
+#ifndef COMMAND_H
+#define COMMAND_H
+#include <string>
+
+#include "MemoryAllocator.h"
+using namespace std;
+
+class Command {
+    string key;
+public:
+    Command(const string& key):key(key) {};
+    virtual ~Command();
+    virtual void execute(MemoryAllocator& MA) = 0;
+};
+class InitCommand : public Command {
+    int size;
+public:
+    InitCommand(int size):Command("INIT"),size(size) {};
+    void execute(MemoryAllocator &MA) override;
+};
+class FreeCommand : public Command {
+    int id;
+public:
+    FreeCommand(int size):Command("FREE"),id(id) {};
+    void execute(MemoryAllocator &MA) override;
+};
+class AllocCommand : public Command {
+    int size;
+public:
+    AllocCommand(int size):Command("ALLOC"),size(size) {};
+    void execute(MemoryAllocator &MA) override;
+};
+class PrintCommand : public Command {
+
+public:
+    PrintCommand(int size):Command("PRINT"){};
+    void execute(MemoryAllocator &MA) override;
+};
+class StatsCommand : public Command {
+
+public:
+    StatsCommand(int size):Command("STATS"){};
+    void execute(MemoryAllocator &MA) override;
+};
+#endif //COMMAND_H
---
+++ src/Commands.cpp
@@ -0,0 +1,4 @@
+//
+// Created by moham on 07/06/2026.
+//
+#include "Commands.h"
